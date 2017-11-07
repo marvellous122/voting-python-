782dc2b37bccf778ef06691dc149be6ff0cb521a
Passion <marvellouspeak@gmail.com>
2017-11-07 07:09:56 -0800

    register choice for admin panel

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
